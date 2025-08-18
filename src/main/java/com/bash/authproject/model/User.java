2e60a289efdcb3ee377476772a1b52e0eb8ba40e
Muhammed Bashir <bashmufol@gmail.com>
2025-08-18 06:50:02 +0100

    updated

---
+++ src/main/java/com/bash/authproject/model/User.java
@@ -0,0 +1,27 @@
+package com.bash.authproject.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    private String email;
+    private String password;
+    private Boolean isActive;
+}
---
+++ src/main/java/com/bash/authproject/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.bash.authproject.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+}
