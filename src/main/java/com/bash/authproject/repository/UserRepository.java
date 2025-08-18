0ccc95fcb59c10f45fe7a7878c0baeba76cd46cd
Muhammed Bashir <bashmufol@gmail.com>
2025-08-18 11:31:21 +0100

    updated

--- src/main/java/com/bash/authproject/repository/UserRepository.java
+++ src/main/java/com/bash/authproject/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.bash.authproject.repository;
 
+import com.bash.authproject.model.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository {
-
+    public User findByUsername(String username);
 }
