ec091b5e50a8ff7f66fd0ba994f6b3279aca581e
Muhammed Bashir <bashmufol@gmail.com>
2025-09-19 22:20:50 +0100

    updated

--- src/test/java/com/bash/authproject/AuthProjectApplicationTests.java
+++ src/test/java/com/bash/authproject/AuthProjectApplicationTests.java
@@ -7,25 +7,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class AuthProjectApplicationTests {
-
-	Calculator calc = new Calculator();
 	@Test
 	void itShouldAddNumbers() {
-//		Given
-		int numberOne = 20;
-		int numberTwo = 30;
-
-//		When
-		int result = calc.add(numberOne, numberTwo);
-
-//		Then
-		assertThat(result).isEqualTo(50);
-	}
-
-	static class Calculator{
-		int add(int a, int b){
-			return a+b;
-		}
 	}
 
 }
